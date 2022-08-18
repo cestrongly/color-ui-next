deb1acf690b7f20f96ee297679100ce06a240cd6
cestrmail@163.com <cestrmail@163.com>
2022-08-18 15:25:46 +0800

    默认国际化为中文简体

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
+<!--
+ * @Author: cest
+ * @Date: 2022-08-18 13:57:11
+ * @LastEditTime: 2022-08-18 15:22:31
+ * @LastEditors: cest
+ * @FilePath: /color-ui-next/src/App.vue
+ * @Description: 编辑描述内容
+-->
 <script setup lang="ts">
 // https://github.com/vueuse/head
 // you can use this to manipulate the document head in any components,
 // they will be rendered correctly in the html results with vite-ssg
 useHead({
-  title: 'Vitesse',
+  title: 'ColorUI Next',
   meta: [
-    { name: 'description', content: 'Opinionated Vite Starter Template' },
+    { name: 'description', content: '基于 antfu/vitesse 构建新一代 ColorUI' },
     {
       name: 'theme-color',
       content: computed(() => isDark.value ? '#00aba9' : '#ffffff'),
@@ -19,6 +27,8 @@ useHead({
     },
   ],
 })
+const { locale } = useI18n()
+locale.value = 'zh-CN'
 </script>
 
 <template>
