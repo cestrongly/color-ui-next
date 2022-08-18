e989e7a8d03e73d26430febca61878a3765047cd
cestrmail@163.com <cestrmail@163.com>
2022-08-18 15:26:06 +0800

    更新文件声明

--- src/shims.d.ts
+++ src/shims.d.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: cest
+ * @Date: 2022-08-18 13:57:11
+ * @LastEditTime: 2022-08-18 14:39:47
+ * @LastEditors: cest
+ * @FilePath: /color-ui-next/src/shims.d.ts
+ * @Description: 腻子脚本 类型声明文件
+ */
 declare interface Window {
   // extend the window
 }
@@ -14,3 +22,8 @@ declare module '*.vue' {
   const component: DefineComponent<{}, {}, any>
   export default component
 }
+
+// declare module 'lodash/cloneDeep'
+// declare module 'element-plus/lib/locale/lang/zh-cn'
+// declare module 'element-plus/lib/locale/lang/en'
+declare module '@intlify/vite-plugin-vue-i18n'
