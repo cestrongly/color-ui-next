5ed46be2ffaed6f6dcb41bcefad4a9b97b7f1f8e
cestrmail@163.com <cestrmail@163.com>
2022-08-18 15:27:00 +0800

    color-ui-next - 定义预定义颜色

---
+++ src/pages/color-ui/index/index.vue
@@ -0,0 +1,11 @@
+<!--
+ * @Author: cest
+ * @Date: 2022-08-18 14:53:38
+ * @LastEditTime: 2022-08-18 14:58:04
+ * @LastEditors: cest
+ * @FilePath: /color-ui-next/src/pages/color-ui/index/index.vue
+ * @Description: 编辑描述内容
+-->
+<template lang="pug">
+.container container
+</template>
