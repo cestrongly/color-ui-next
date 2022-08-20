779b553dfb2d7f01567892e3b62a2a3bdbdaf56a
不会淹死的鱼 <cestrmail@163.com>
2022-08-21 07:14:25 +0800

    消除 uno.css 的影响

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
+/*
+ * @Author: cest
+ * @Date: 2022-08-20 19:22:16
+ * @LastEditTime: 2022-08-21 07:09:11
+ * @LastEditors: cest
+ * @FilePath: /color-ui-next/src/main.ts
+ * @Description: 编辑描述内容
+ */
 import { ViteSSG } from 'vite-ssg'
 import { setupLayouts } from 'virtual:generated-layouts'
 import App from './App.vue'
 import type { UserModule } from './types'
 import generatedRoutes from '~pages'
 
-import '@unocss/reset/tailwind.css'
-import './styles/main.css'
-import 'uno.css'
+// import '@unocss/reset/tailwind.css'
+// import './styles/main.css'
+// import 'uno.css'
 
 const routes = setupLayouts(generatedRoutes)
 
--- unocss.config.ts
+++ unocss.config.ts
@@ -1,35 +1,35 @@
 /*
  * @Author: cest
  * @Date: 2022-08-18 13:57:11
- * @LastEditTime: 2022-08-18 15:09:16
+ * @LastEditTime: 2022-08-21 07:04:12
  * @LastEditors: cest
  * @FilePath: /color-ui-next/unocss.config.ts
  * @Description: 编辑描述内容
  */
 import {
   defineConfig,
-  presetAttributify,
+  // presetAttributify,
   presetIcons,
-  presetTypography,
-  presetUno,
+  // presetTypography,
+  // presetUno,
   // presetWebFonts,
   transformerDirectives,
   transformerVariantGroup,
 } from 'unocss'
 
 export default defineConfig({
-  shortcuts: [
-    ['btn', 'px-4 py-1 rounded inline-block bg-teal-700 text-white cursor-pointer hover:bg-teal-800 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
-    ['icon-btn', 'inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-teal-600'],
-  ],
+  // shortcuts: [
+  //   ['btn', 'px-4 py-1 rounded inline-block bg-teal-700 text-white cursor-pointer hover:bg-teal-800 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
+  //   ['icon-btn', 'inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-teal-600'],
+  // ],
   presets: [
-    presetUno(),
-    presetAttributify(),
+    // presetUno(),
+    // presetAttributify(),
     presetIcons({
       scale: 1.2,
       warn: true,
     }),
-    presetTypography(),
+    // presetTypography(),
     // presetWebFonts({
     //   fonts: {
     //     sans: 'DM Sans',
